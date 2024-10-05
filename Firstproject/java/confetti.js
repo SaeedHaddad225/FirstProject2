b6150dae0d683430a7eca051f32ca88734f7c7da
SaeedHaddad225 <saeedhaddad225@gmail.com>
2024-10-05 08:19:17 +0300

    Delete Firstproject directory

--- Firstproject/java/confetti.js
+++
@@ -1,9 +0,0 @@
-const button = document.querySelector('#random');
-const canvas = document.querySelector('#confetti');
-
-const jsConfetti = new JSConfetti();
-
-setTimeout (button.addEventListener('click', () => {jsConfetti.addConfetti(
-    {}
-)
-}), 4000 )
